de01ade198f5d8d735e4fe42e7674040a490860d
Shounak Amladi <shounak.p.amladi@gmail.com>
2023-05-09 22:35:20 -0400

    Revert "new website"
    This reverts commit 24464c736e9c1f127287d311693e3781c757bfc3.

---
+++ src/components/nak.tsx
@@ -0,0 +1,19 @@
+import { Motion } from '@motionone/solid';
+import { spring } from 'motion';
+import './nak.css';
+import { Show, createSignal } from 'solid-js';
+
+export function Nak() {
+   const [hovering, setHovering] = createSignal(false);
+
+   return (
+      <Motion.h1 hover={{ scale: 1.2 }} transition={{ delay: 0.05, easing: spring({ damping: 6 }) }} onMouseEnter={() => setHovering(() => true)} onMouseLeave={() => setHovering(() => false)}>
+         <a href="/">
+            <Show when={hovering()}>
+               <span class="text">Shou</span>
+            </Show>
+            <span class="text-gradient">nak</span>
+         </a>
+      </Motion.h1>
+   );
+ }
