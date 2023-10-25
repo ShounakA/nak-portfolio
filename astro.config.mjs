186a0da1df9d7e9b0bec652adf85fa6b4d933e74
Shounak Amladi <shounakamladi@cmail.carleton.ca>
2023-10-24 20:28:05 -0400

    feat: adds blog layout plus first post

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,16 @@
 import { defineConfig } from 'astro/config';
 import solidJs from "@astrojs/solid-js";
+import remarkToc from 'remark-toc';
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs()],
+  integrations: [solidJs(), 
+    mdx({
+      syntaxHighlight: 'shiki',
+      shikiConfig: { theme: 'github-dark' },
+      remarkPlugins: [remarkToc],
+      remarkRehype: { footnoteLabel: 'Footnotes' },
+      gfm: false,
+    })]
 });
